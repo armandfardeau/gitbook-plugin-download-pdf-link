c6077d00750cae45d0ddd64b9b17caab0581c840
armandfardeau <fardeauarmand@gmail.com>
2018-07-29 23:15:36 +0200

    add new type for base

--- book/plugin.js
+++ book/plugin.js
@@ -1,25 +1,26 @@
-require(["gitbook", "jQuery"], function(gitbook, $) {
-  gitbook.events.bind('start', function (e, config) {
-    var conf = config['download-pdf-link'];
-    var lang = gitbook.state.innerLanguage;
-    var label = conf.label;
-    if (lang) {
-       // label can be a unique string for multi-languages site
-       if (typeof label === 'object') label = label[lang];
-   }
-    var base = conf.base
-    var multilingual = conf.multilingual || false
+require(["gitbook", "jQuery"], function (gitbook, $) {
+    gitbook.events.bind('start', function (e, config) {
+        const conf = config['download-pdf-link'];
+        const lang = gitbook.state.innerLanguage;
+        let label = conf.label;
+        let base = conf.base;
+        if (lang) {
+            // label can be a unique string for multi-languages site
+            if (typeof label === 'object') label = label[lang];
+            if (typeof base === 'object') base = base[lang];
+        }
+        const multilingual = conf.multilingual || false
 
-    if (base.slice(-1) !== "/") {
-      base += "/"
-    }
+        if (base.slice(-1) !== "/") {
+            base += "/"
+        }
 
-    gitbook.toolbar.createButton({
-      icon: 'fa fa-file-pdf-o',
-      text: label,
-      onClick: function() {
-        window.open(base + "?lang=" + lang)
-      }
+        gitbook.toolbar.createButton({
+            icon: 'fa fa-file-pdf-o',
+            text: label,
+            onClick: function () {
+                window.open(base)
+            }
+        })
     })
-  })
 })
